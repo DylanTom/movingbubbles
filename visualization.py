5888549e35f79bf462edd52b5c4f593ab2677be6
Benjamin Tang <bt283@cornell.edu>
2023-11-11 18:56:40 -0500

    added dylan clara justin

--- visualization.py
+++ visualization.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 # Load d3blocks
 from d3blocks import D3Blocks
+
 #
 # Initialize
 d3 = D3Blocks()
@@ -10,5 +11,26 @@ d3 = D3Blocks()
 df = pd.read_csv("input.csv")
 #
 # Plot
-d3.movingbubbles(df, speed={"slow": 10, "medium": 2, "fast": 1}, filepath='movingbubbles.html')
-#
+d3.movingbubbles(
+    df,
+    speed={"slow": 0, "medium": 20, "fast": 10},
+    filepath="movingbubbles.html",
+)
+# 0 = Dylan
+# 1 = Clara
+# 2 = Justin
+
+
+# Meeting
+# Aleep
+# Class
+# Wake Up and Think About Life
+# Wake Up
+# Cook
+# Eat
+# In Crossings
+# In Dryden
+# Losing Money
+# Having a Crisis
+# Getting Drunk
+# DTI Stuff
